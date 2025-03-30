f2e4f28fa080819e338adcc3736103f9333781ce
ja9-z <janine.zeng.6@gmail.com>
2025-03-30 02:52:55 -0400

    moved file

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template, flash,jsonify
+from flask import Flask, redirect, url_for, request, render_template, flash, jsonify
 import pandas as pd
 import numpy as np
 
@@ -13,6 +13,7 @@ import spotify_recommendation
 
 movie_name = ""
 
+
 # Flask constructor
 app = Flask(__name__) 
 CORS(app)  
@@ -23,8 +24,8 @@ def receive_movie():
 
     print("Received movie:", movie_name)
     print(movie_recommendation.get_recommendations(movie_name, movie_recommendation.cosine_sim2))  # Print to console
-    return "Succes"
-
+    return "success"
+"""
     if(request.method == "POST"):
         #can check if request.form["nm"] not blank, do more stuff with it
         movie_name = data.get("movie", "").strip()
@@ -34,12 +35,12 @@ def receive_movie():
         return render_template("login.html")
     #data.get("movie")
     #return redirect(url_for("motion", mv = data.get("movie")))
-"""
+
     print("Received movie:", data.get("movie"))  # Print to console
-    return "Success"""
+    return "Success""""""
 
 
-"""
+
 @app.route("/", methods = ["POST", "GET"])
 def home():
     if(request.method == "POST"):
@@ -47,21 +48,29 @@ def home():
         user = request.form["nm"]
         return redirect(url_for("user", usr = user))
     else:
-        return render_template("login.html")"
+        return render_template("login.html")
         """
 
-@app.route("/getRecs")
-def getRecs(mv):
+@app.route("/getMovieRecs")
+def getMovRecs():
+    movies = movie_recommendation.get_recommendations("interstellar", movie_recommendation.cosine_sim2)
+    
+    return jsonify({"movies": movies})
+
+    """
     MovieRecommendations = movie_recommendation.get_recommendations(movie_name, movie_recommendation.cosine_sim2)
     if MovieRecommendations:
         return render_template('recommendation.html', movies=MovieRecommendations, songs=spotify_recommendation.get_recommendation(movie_name))
     else:
         return render_template('recommendation.html', movies=[], message="NO MOTION DETECTED")
+        """
+
+@app.route("/getSongRecs")
+def getSongRecs():
+    songs=spotify_recommendation.get_recommendation(movie_name)
+    return jsonify({"songs": songs})
 
 
-#@app.route("/<usr>/spot")
-#def user(usr):
-    
 
 if __name__=='__main__':
     app.run(debug = True)
